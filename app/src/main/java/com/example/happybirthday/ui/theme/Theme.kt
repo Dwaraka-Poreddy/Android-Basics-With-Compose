9718ce622c13c1e913914f658806c686ac10e210
dwarakanatha.poreddy <dwarakanatha.poreddy@gojek.com>
2026-02-02 22:24:50 +0530

    Fix system status bar colors

--- app/src/main/java/com/example/happybirthday/ui/theme/Theme.kt
+++ app/src/main/java/com/example/happybirthday/ui/theme/Theme.kt
@@ -139,7 +139,7 @@ fun WoofTheme(
         SideEffect {
             val window = (view.context as Activity).window
             window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
 
