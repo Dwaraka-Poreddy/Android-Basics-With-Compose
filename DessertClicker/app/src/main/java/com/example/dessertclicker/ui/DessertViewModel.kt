1dc13e43788c481eea3d7e2fe79a44032c839809
dwarakanatha.poreddy <dwarakanatha.poreddy@gojek.com>
2026-02-21 18:01:54 +0530

    Refactor DessertClicker app with ViewModel

---
+++ DessertClicker/app/src/main/java/com/example/dessertclicker/ui/DessertViewModel.kt
@@ -0,0 +1,40 @@
+package com.example.dessertclicker.ui
+
+import androidx.lifecycle.ViewModel
+import com.example.dessertclicker.data.DessertUiState
+import com.example.dessertclicker.data.Datasource.dessertList
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class DessertViewModel : ViewModel() {
+    private val _dessertUiState = MutableStateFlow(DessertUiState())
+    val dessertUiState: StateFlow<DessertUiState>  = _dessertUiState.asStateFlow()
+
+    fun onDessertClicked() {
+        _dessertUiState.update { cupcakeUiState ->
+            val dessertSold = cupcakeUiState.dessertsSold + 1
+            val nextDessertIndex = determineDessertIndex(dessertSold)
+            cupcakeUiState.copy(
+                currentDessertIndex =  nextDessertIndex,
+                revenue = cupcakeUiState.revenue + cupcakeUiState.currentDessertPrice,
+                dessertsSold = dessertSold,
+                currentDessertImageId = dessertList[nextDessertIndex].imageId,
+                currentDessertPrice = dessertList[nextDessertIndex].price
+            )
+        }
+    }
+
+    private fun determineDessertIndex(dessertsSold: Int) : Int {
+        var dessertIndex = 0
+        for (index in dessertList.indices) {
+            if(dessertsSold >= dessertList[index].startProductionAmount) {
+                dessertIndex = index
+            } else {
+                break
+            }
+        }
+        return dessertIndex
+    }
+}
