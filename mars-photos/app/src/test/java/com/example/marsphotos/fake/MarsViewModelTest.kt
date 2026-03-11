b15c5a9f82767c4eefbfec665d8b4898c3879de1
dwarakanatha.poreddy <dwarakanatha.poreddy@gojek.com>
2026-03-11 08:50:03 +0530

    Add load and display images from itnernet in MarsPhotosApp

---
+++ mars-photos/app/src/test/java/com/example/marsphotos/fake/MarsViewModelTest.kt
@@ -0,0 +1,25 @@
+package com.example.marsphotos.fake
+
+import com.example.marsphotos.rules.TestDispatcherRule
+import com.example.marsphotos.ui.screens.MarsUiState
+import com.example.marsphotos.ui.screens.MarsViewModel
+import kotlinx.coroutines.test.runTest
+import org.junit.Rule
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class MarsViewModelTest {
+    @get:Rule
+    val testDispatcher = TestDispatcherRule()
+
+    @Test
+    fun marsViewModel_getMarsPhotos_verifyMarsUiStateSuccess() = runTest {
+        val marsViewModel = MarsViewModel(
+            marsPhotosRepository = FakeNetworkMarsPhotosRepository()
+        )
+        assertEquals(
+            MarsUiState.Success(FakeDataSource.photosList),
+            marsViewModel.marsUiState
+        )
+    }
+}
---
+++ mars-photos/app/src/test/java/com/example/marsphotos/fake/NetworkMarsRepositoryTest.kt
@@ -0,0 +1,16 @@
+package com.example.marsphotos.fake
+
+import com.example.marsphotos.data.NetworkMarsPhotosRepository
+import kotlinx.coroutines.test.runTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class NetworkMarsRepositoryTest {
+    @Test
+    fun networkMarsPhotosRepository_getMarsPhotos_verifyPhotoList() = runTest{
+        val repository = NetworkMarsPhotosRepository(
+            marsApiService = FakeMarsApiService()
+        )
+        assertEquals(FakeDataSource.photosList, repository.getMarsPhotos())
+    }
+}
